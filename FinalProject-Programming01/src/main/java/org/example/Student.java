cf2ed81752546d53046cfb4674bc50e99ac33267
DavidSabb <158499251+DavidSabb@users.noreply.github.com>
2024-05-26 23:39:49 -0400

    Created Student class
    Created Student Class. Wrote the fields for the class, wrote the methods, getters, setters and tostring

---
+++ FinalProject-Programming01/src/main/java/org/example/Student.java
@@ -0,0 +1,123 @@
+package org.example;
+import java.util.ArrayList;
+
+public class Student {
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private ArrayList<Course> registeredCourses;
+    private static int nextId = 1;
+
+    public Student(String studentName, Gender gender, Address address, Department department) {
+        this.studentName = studentName;
+        this.gender = gender;
+        this.address = address;
+        this.department = department;
+        this.studentId = generateStudentId();
+        this.registeredCourses = new ArrayList<>();
+    }
+
+    private String generateStudentId() {
+        char prefix = 'S';
+        return prefix + Integer.toString(nextId++);
+    }
+
+    public boolean registerCourse(Course course) {
+        if (registeredCourses.contains(course)) {
+            return false;
+        }
+        registeredCourses.add(course);
+        course.getRegisteredStudents().add(this);
+        course.registerStudent(this);
+        return true;
+    }
+
+    public boolean dropCourse(Course course) {
+        if (!registeredCourses.contains(course)) {
+            return false;
+        }
+        registeredCourses.remove(course);
+        course.getRegisteredStudents().remove(this);
+        course.removeStudent(this);
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId='" + studentId + '\'' +
+                ", studentName='" + studentName + '\'' +
+                ", gender=" + gender +
+                ", address=" + address +
+                ", department=" + department +
+                ", registeredCourses=" + registeredCourses +
+                '}';
+    }
+
+    public boolean equals(Student o) {
+        if (studentId.equals(o.studentId) && studentName.equals(o.studentName) && gender == o.gender
+        && address.equals(o.address) && department.equals(o.department) && registeredCourses == o.registeredCourses){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public ArrayList<Course> getRegisteredCourses() {
+        return registeredCourses;
+    }
+
+    public void setRegisteredCourses(ArrayList<Course> registeredCourses) {
+        this.registeredCourses = registeredCourses;
+    }
+
+    public static int getNextId() {
+        return nextId;
+    }
+
+    public static void setNextId(int nextId) {
+        Student.nextId = nextId;
+    }
+}
